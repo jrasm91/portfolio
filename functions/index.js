b78f29b07c7b28c7de1ac246ab2047a530c78180
Jason Rasmussen <jason@pravici.com>
2021-02-11 13:43:43 -0700

    Remove functions

--- functions/index.js
+++
@@ -1,8 +0,0 @@
-const functions = require('firebase-functions');
-require('dotenv').config();
-
-const { slackIntegration } = require('./slackIntegration');
-
-module.exports = {
-  slackIntegration: functions.pubsub.schedule('every day 09:00').onRun(slackIntegration)
-};
